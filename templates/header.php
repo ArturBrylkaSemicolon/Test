25e69b7b30a214c91468ae4f48d5a39b273b20f1
ArturBrylkaSemicolon <artur.brylka.semicolon@gmail.com>
2024-12-13 17:23:21 +0100

    Initial commit

---
+++ templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="https://gmpg.org/xfn/11"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <div class="top-sections">
+                         
+            <header>
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="main-menu">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'main-menu',
+                            'container' => false,
+                            'menu_class' => '',
+                            'fallback_cb' => '__return_false',
+                            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                            'depth' => 2,
+                            'walker' => new bootstrap_5_wp_nav_menu_walker()
+                        ));
+                        ?>
+                    </div>
+                </div>
+            </nav>
+            </header>                   
+            
+            <section class="search">
+                <div class="top-section-text">Text</div>
+                <div class="search-inputs">Search Inputs</div>
+            </section>
+        </div>
